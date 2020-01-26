a20fd88712e30216d7ef1792319f8db8a3d1c39e
Sema4Texas <tech@sema4texas.com>
2020-01-26 21:41:14 +0000

    Setup fully functional letsencrypt container

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
+# Note: 
+# The --standalone flag spins up a temporary webserver,
+# that allows certbot to complete the ACME challenge.
+# This is why we need to docker-compose down before
+# running the container to free up port 80.
+# The "-u" argument makes the container run as UID 33, 
+# which is the www-data user on Ubuntu used by Nginx.
+# We don't need a random webserver running as root, eh?
+
 docker-compose down;
 
 docker run -it --rm --name certbot \
   -v /spoke/letsencrypt:/etc/letsencrypt \
-  -v /var/log/letsencrypt:/var/log/letsencrypt \
-  -p 80:80
-  letsencrypt/letsencrypt -t certonly \
-  --dry-run \
+  -v /spoke/var/log/letsencrypt:/var/log/letsencrypt \
+  -p 80:80 \
+  -u 33 \
+  certbot/certbot -t certonly \
+  --standalone \
+  --email tech@sema4texas.com \
+  --no-eff-email \
   --agree-tos --renew-by-default \
   -d text.sema4texas.com;
 
 docker-compose up -d
-  
